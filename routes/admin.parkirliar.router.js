53049a49480af780c000f2de36e1949bf2fe541b
Awan Ardy <muhammadawan46@gmail.com>
2025-05-18 20:17:48 +0800

    Create admin.parkirliar.router.js

---
+++ routes/admin.parkirliar.router.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllLaporan,
+  addLaporan,
+  updateLaporan,
+  getLaporanById,
+  deleteParkir,
+} = require("../controllers/admin.parkirliar.controller");
+
+const {
+  verifyUser,
+  isAdmin,
+  authenticateToken,
+} = require("../middleware/auth.router");
+const upload = require("../middleware/upload");
+
+router.get(
+  "/adminparkir",
+  authenticateToken,
+  verifyUser,
+  isAdmin,
+  getAllLaporan,
+);
+router.get("/adminparkir/detail/:id", getLaporanById);
+router.post("/admin-parkir", upload.single("bukti"), addLaporan);
+router.put("/adminparkir/:id", upload.single("bukti"), updateLaporan);
+router.delete("/adminparkir/:id", upload.single("bukti"), deleteParkir);
+
+// admin approval
+const { approveParkirLiar } = require("../middleware/adminApproval");
+router.post("/admin-parkir/:id", approveParkirLiar);
+
+module.exports = router;
